b3f2070beca117ea3694e8de3837959be83a247c
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-06-20 01:54:52 -0400

    added config for firebase

--- 1-fast-but-not-good-setup/start/src/config.js
+++ 1-fast-but-not-good-setup/start/src/config.js
@@ -1,5 +1,10 @@
 export const config = {
-  firebase: {
-    // Go to the console to get your configuration object
-  },
+  firebase:  {
+    apiKey: "AIzaSyBvcipQdpCLa5vn4xKQcouDFZrNX-SD5ec",
+    authDomain: "workshop-frontendmasters.firebaseapp.com",
+    projectId: "workshop-frontendmasters",
+    storageBucket: "workshop-frontendmasters.appspot.com",
+    messagingSenderId: "49225368064",
+    appId: "1:49225368064:web:bac8b4d2a9db8e28140ee5"
+  }
 };
